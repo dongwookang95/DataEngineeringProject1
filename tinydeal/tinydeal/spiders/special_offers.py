748e10d168253da42af2e6674bc5f0d41e3d9c8e
dongwookang <d.kang-1@student.tudelft.nl>
2020-04-29 12:47:57 +0200

    building a spider

--- tinydeal/tinydeal/spiders/special_offers.py
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -4,8 +4,14 @@ import scrapy
 
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
-    allowed_domains = ['www.tinydeal.com.hk/specials.html']
-    start_urls = ['http://www.tinydeal.com.hk/specials.html/']
+    allowed_domains = ['www.tinydeal.com.hk']
+    start_urls = ['https://www.tinydeal.com.hk/specials.html']
 
     def parse(self, response):
-        pass
+        for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
+            yield{
+                'title' : product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url' : response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
+                'discounted_price' : product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
+                'original_price' : product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
+            }
