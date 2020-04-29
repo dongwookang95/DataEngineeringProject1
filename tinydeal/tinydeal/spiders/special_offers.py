a37e80cf50da385b76a4f9051291cb40bd07a90c
dongwookang <d.kang-1@student.tudelft.nl>
2020-04-29 13:16:40 +0200

    crawl multiple pages

--- tinydeal/tinydeal/spiders/special_offers.py
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -4,7 +4,7 @@ import scrapy
 
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
-    allowed_domains = ['www.tinydeal.com.hk']
+    allowed_domains = ['www.tinydeal.com']
     start_urls = ['https://www.tinydeal.com.hk/specials.html']
 
     def parse(self, response):
@@ -14,4 +14,8 @@ class SpecialOffersSpider(scrapy.Spider):
                 'url' : response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
                 'discounted_price' : product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
                 'original_price' : product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
-            }
+            }  
+        next_page = response.xpath("//a[@class='nextPage']/@href").get()
+
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
