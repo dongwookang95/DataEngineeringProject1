12315eaf58adb674d8167b76af86b4e2b3470ab1
dongwookang <d.kang-1@student.tudelft.nl>
2020-05-07 23:32:58 +0200

    add next page rule

--- imdb/imdb/spiders/best_movies.py
+++ imdb/imdb/spiders/best_movies.py
@@ -7,15 +7,19 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['http://imdb.com/']
+    start_urls = ['https://www.imdb.com/search/title/?groups=top_250&sort=user_rating']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']"), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[1]")),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield{
+            'title' : response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'year' : response.xpath("//div[@class='title_wrapper']/h1/span/text()").get(),
+            'duration' : response.xpath("normalize-space(//div[@class='subtext']/time/text())").get(),
+            'genre' : response.xpath("//div[@class='title_wrapper']/div/a/text()").get(),
+            'rating' : response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            'movie_url' : response.url
+        }
